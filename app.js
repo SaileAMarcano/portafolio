ff0cc5bcc369954b265b05050dd00062094cae40
SaileAMarcano <saileamrcno@gmail.com>
2026-03-05 21:10:26 -0500

    Boton Hablemos, activo.

--- app.js
+++ app.js
@@ -7,3 +7,22 @@ menuToggle.addEventListener('click', () => {
     // classList.toggle agrega la clase 'active' si no la tiene, y se la quita si ya la tiene.
     navLinks.classList.toggle('active');
 });
+
+const btnHablemos = document.querySelector('a[href="#contacto"]');
+const modalOverlay = document.getElementById('modal-contacto');
+const btnCerrar = document.getElementById('btn-cerrar');
+
+btnHablemos.addEventListener('click', (e) => {
+    e.preventDefault(); 
+    modalOverlay.classList.add('active');
+});
+
+btnCerrar.addEventListener('click', () => {
+    modalOverlay.classList.remove('active');
+});
+
+window.addEventListener('click', (e) => {
+    if (e.target === modalOverlay) {
+        modalOverlay.classList.remove('active');
+    }
+});
