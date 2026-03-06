bbb40678ed54ca0838dca4f59470954d599d9ba5
SaileAMarcano <saileamrcno@gmail.com>
2026-03-05 21:41:55 -0500

    Detalles llamativos

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
 
+const botonesHablemos = document.querySelectorAll('.abrir-modal');
+const modalOverlay = document.getElementById('modal-contacto');
+const btnCerrar = document.getElementById('btn-cerrar');
+
+
 menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
-const btnHablemos = document.querySelector('a[href="#contacto"]');
-const modalOverlay = document.getElementById('modal-contacto');
-const btnCerrar = document.getElementById('btn-cerrar');
 
-btnHablemos.addEventListener('click', (e) => {
-    e.preventDefault(); 
-    modalOverlay.classList.add('active');
+botonesHablemos.forEach(boton => {
+    boton.addEventListener('click', (e) => {
+        e.preventDefault();
+        modalOverlay.classList.add('active');
+        navLinks.classList.remove('active');
+    });
 });
 
 btnCerrar.addEventListener('click', () => {
