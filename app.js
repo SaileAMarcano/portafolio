0fcd66684e1b26b2f7b01e1402d9e77ccb061e69
SaileAMarcano <saileamrcno@gmail.com>
2026-03-05 21:12:11 -0500

    detallitos

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-// 1. Seleccionamos los elementos de nuestro HTML
 const menuToggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
 
-// 2. Agregamos el evento de "clic"
 menuToggle.addEventListener('click', () => {
-    // classList.toggle agrega la clase 'active' si no la tiene, y se la quita si ya la tiene.
     navLinks.classList.toggle('active');
 });
 
